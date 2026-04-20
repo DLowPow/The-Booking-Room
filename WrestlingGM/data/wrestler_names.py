b65f9f4c4f183d024fb7f43f77ef6b39ed6324ab
DLowPow-FWE <157801179+DLowPow@users.noreply.github.com>
2026-04-20 20:08:22 +0200

    Create wrestler_names.py

---
+++ WrestlingGM/data/wrestler_names.py
@@ -0,0 +1,37 @@
+"""
+Wrestler Name Pools - Minimal Test
+"""
+
+FIRST_NAMES_MALE = [
+    "Adam", "Alex", "Bob", "Chris", "Dan",
+]
+
+FIRST_NAMES_FEMALE = [
+    "Anna", "Beth", "Carol", "Diana", "Emma",
+]
+
+LAST_NAMES = [
+    "Smith", "Jones", "Brown", "White", "Black",
+]
+
+NICKNAMES_BY_TIER = {
+    1: [None, "The Kid"],
+    2: [None, "The Underdog"],
+    3: [None, "The Future"],
+    4: [None, "The Ace"],
+    5: ["The Legend"],
+}
+
+HOMETOWNS = [
+    "New York City",
+    "Los Angeles",
+    "Chicago",
+]
+
+FINISHER_NAMES = [
+    "The Shutdown", "Lights Out", "Final Chapter",
+]
+
+SIGNATURE_MOVES = [
+    "Suplex", "Dropkick", "Clothesline",
+]
